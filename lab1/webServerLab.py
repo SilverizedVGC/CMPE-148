16285627fdfaf04e84eea246c49ece8453a70d2f
SilverizedVGC <pikacuber@gmail.com>
2026-02-28 23:41:29 -0800

    Finish coding perhaps?

--- lab1/webServerLab.py
+++ lab1/webServerLab.py
@@ -1,26 +1,29 @@
 #import socket module
+from modulefinder import test
 from socket import *
 import sys # In order to terminate the program
 
 serverSocket = socket(AF_INET, SOCK_STREAM)
 #Prepare a sever socket
-serverSocket.bind(('', 8888))
+serverSocket.bind(('127.0.0.1', 8888))
 serverSocket.listen(1)
 while True:
     #Establish the connection
     print('Ready to serve...')
     connectionSocket, addr = serverSocket.accept()
-    #Fill in start
-    #Fill in end
     try:
         message = connectionSocket.recv(1024).decode()
         filename = message.split()[1]
+
+        if filename == '/':
+            filename = '/index.html'
+
         f = open(filename[1:])
         outputdata = f.read()
         #Send one HTTP header line into socket
-        serverSocket.send("200 OK\r\n".encode())
-        #Fill in start
-        #Fill in end
+        connectionSocket.send("HTTP/1.1 200 OK\r\n".encode())
+        connectionSocket.send("Content-Type:text/html\r\n".encode())
+        connectionSocket.send("\r\n".encode())
         #Send the content of the requested file to the client
         for i in range(0, len(outputdata)):
             connectionSocket.send(outputdata[i].encode())
@@ -29,12 +32,15 @@ while True:
         connectionSocket.close()
     except IOError:
         #Send response message for file not found
-        serverSocket.send("404 Not Found\r\n".encode())
-        #Fill in start
-        #Fill in end
+        connectionSocket.send("HTTP/1.1 404 Not Found\r\n".encode())
+        connectionSocket.send("Content-Type:text/html\r\n".encode())
+        connectionSocket.send("\r\n".encode())
+
+        error_msg = "<html><h1>404 Not Found</h1></html>"
+
+        connectionSocket.send(error_msg.encode())
+
         #Close client socket
         connectionSocket.close()
-        #Fill in start
-        #Fill in end
 serverSocket.close()
 sys.exit()#Terminate the program after sending the corresponding data 
